388557987addf4fe4ff4423c8456a3d0966ee4c8
Nguyễn Trung Nam <vuilachinh66@gmail.com>
2019-12-10 11:24:14 +0700

    Login function + Layout

---
+++ Weedkend/Weedkend/Pages/Login.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Weedkend.Models;
+
+namespace Weedkend
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+        public IActionResult OnPostLogin(string username, string password)
+        {
+            using (var context = new MyContext())
+            {
+                var user = context.Set<Account>().FirstOrDefault(u => u.UserName == username && u.Password == password && u.Status.Equals("active"));
+                var role = context.Set<Role>().FirstOrDefault(r => r.RoleId == user.Role);
+                if (user == null)
+                {
+                    TempData["ERROR"] = "Invalid username password!";
+                    return Page();
+                }
+                else if (role.RoleName.Equals("admin"))
+                {
+                    return Redirect("/homeadmin");
+                }
+                else return Redirect("/homepage");
+            }
+        }
+    }
+}
